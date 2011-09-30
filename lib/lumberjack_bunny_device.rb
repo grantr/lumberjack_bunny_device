34284245cbb0def9d575d16b97fa0ed26e5459ae
Grant Rodgers <grantr@gmail.com>
2011-09-30 16:04:44 -0700

    simple bunny device

--- lib/lumberjack_bunny_device.rb
+++ lib/lumberjack_bunny_device.rb
@@ -1,5 +1,50 @@
 require "lumberjack_bunny_device/version"
+require 'lumberjack'
+require 'bunny'
 
-module LumberjackBunnyDevice
-  # Your code goes here...
+module Lumberjack
+  class Device
+    class Bunny < Device
+
+      attr_accessor :options
+
+      def initialize(options={})
+        options = options.dup
+        @exchange_name = options.delete(:exchange)
+        @routing_key = options.delete(:routing_key)
+        @options = options
+
+        @lock = ::Mutex.new
+      end
+
+      def write(entry)
+        @lock.synchronize do
+          exchange.publish(entry.message, :key => @routing_key)
+        end
+      end
+
+      def close
+        flush
+        @lock.synchronize do
+          bunny.stop if bunny 
+        end
+      end
+
+      def flush
+        
+      end
+
+      def bunny
+        unless @bunny
+          @bunny = ::Bunny.new(options)
+          @bunny.start
+        end
+        @bunny
+      end
+
+      def exchange
+        @exchange ||= bunny.exchange(@exchange_name)
+      end
+    end
+  end
 end
--- lumberjack_bunny_device.gemspec
+++ lumberjack_bunny_device.gemspec
@@ -21,4 +21,6 @@ Gem::Specification.new do |s|
   # specify any dependencies here; for example:
   # s.add_development_dependency "rspec"
   # s.add_runtime_dependency "rest-client"
+  s.add_runtime_dependency "lumberjack", ["~> 1.0"]
+  s.add_runtime_dependency "bunny", ["~> 0.7.5"]
 end
